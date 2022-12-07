12e2a269ad933cf769e132d47f405e990d5fec39
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-07 00:03:41 -0500

    feat(internal): `ensurePosix`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/internal/__tests__/ensure-posix.spec.ts
@@ -0,0 +1,40 @@
+/**
+ * @file Unit Tests - ensurePosix
+ * @module pathe/internal/tests/unit/ensurePosix
+ */
+
+import delimiter from '#src/lib/delimiter'
+import sep from '#src/lib/sep'
+import testSubject from '../ensure-posix'
+
+describe('unit:internal/ensurePosix', () => {
+  let path: string
+
+  beforeEach(() => {
+    path = 'C:\\Windows\\system32;C:\\Windows;C:\\Program Files\\node\\'
+  })
+
+  it('should consolidate duplicate posix path segment separators', () => {
+    expect(testSubject(path)).to.not.include('//')
+  })
+
+  it('should convert windows path delimiters', () => {
+    expect(testSubject(path)).to.not.include(';')
+  })
+
+  it('should convert windows path segment separators', () => {
+    expect(testSubject(path)).to.not.include('\\')
+  })
+
+  it('should return path that meets posix standards', () => {
+    // Arrange
+    const segments: string[] = [
+      `C:${sep}Windows${sep}system32`,
+      `C:${sep}Windows`,
+      `C:${sep}Program Files${sep}node${sep}`
+    ]
+
+    // Act + Expect
+    expect(testSubject(path)).to.equal(segments.join(delimiter))
+  })
+})
